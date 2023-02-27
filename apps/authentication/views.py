b8650eb9454fddc256361f0f3081b55316d84b7c
unknown <admin@apsaid.com.pe>
2023-02-27 11:12:26 -0500

    creacion de LoginView para la authenticacion del estudiante y devolver su respectivo JWT

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
 
-# Create your views here.
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = User.objects.filter(username=username).first()
+
+        if user is None:
+            return Response({'error': 'Invalid username'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not user.check_password(password):
+            return Response({'error': 'Invalid password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = TokenObtainPairSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
